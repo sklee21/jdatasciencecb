f8d77ec8fd674a407ee973dc6410b692362dff34
uxl21 <uxl21@nate.com>
2020-12-03 17:37:32 +0900

    Change small things...

--- src/main/java/net/uxl21/jdatasciencecb/App.java
+++ src/main/java/net/uxl21/jdatasciencecb/App.java
@@ -4,10 +4,19 @@ package net.uxl21.jdatasciencecb;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+	
+    public static void main(String[] args) {
+    	String classToRun;
+    	
+    	if( args.length == 0 ) {
+    		classToRun = "App";
+    		
+    	} else {
+    		classToRun = args[0];
+    	}
+    	
+        System.out.println( "Hello World! : " + classToRun );
     }
+    
 }
