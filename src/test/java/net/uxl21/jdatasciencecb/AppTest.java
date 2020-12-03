59f190872a655ecd70745447e3e7e918430732b5
uxl21 <uxl21@nate.com>
2020-12-03 17:10:22 +0900

    Initial commit

---
+++ src/test/java/net/uxl21/jdatasciencecb/AppTest.java
@@ -0,0 +1,20 @@
+package net.uxl21.jdatasciencecb;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
