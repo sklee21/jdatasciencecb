3f1eab4848e05fb035a5aef97ed7f54a2e4d5b2c
uxl21 <uxl21@nate.com>
2020-12-04 18:18:26 +0900

    1. Determine a class to run by arguments.

--- src/main/java/net/uxl21/jdatasciencecb/App.java
+++ src/main/java/net/uxl21/jdatasciencecb/App.java
@@ -1,21 +1,67 @@
 package net.uxl21.jdatasciencecb;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
+
 /**
  * Hello world!
  *
  */
 public class App {
 	
+	public static final String DEFAULT_PACKAGE = "net.uxl21.jdatasciencecb.";
+	
+	
     public static void main(String[] args) {
-    	String classToRun;
+    	String classToRun = DEFAULT_PACKAGE;
+    	String[] params;
     	
     	if( args.length == 0 ) {
-    		classToRun = "App";
+    		classToRun += "collectclean.TestRecursiveDirectoryTraversal";
+    		params = new String[0];
+    		
+    	} else if ( args.length == 1 ) {
+    		classToRun += args[0];
+    		params = new String[0];
+    		
     	} else {
-    		classToRun = args[0];
+    		classToRun += args[0];
+   			params = Arrays.copyOfRange(args, 1, args.length - 1);
     	}
     	
         System.out.println( "Hello World! : " + classToRun );
+        
+        try {
+        	 Constructor constructor = Class.forName(classToRun).getConstructor(null);
+        	 System.out.println( "Constructor : " + constructor );
+        	 
+        	 if( constructor != null ) {
+        		 
+        		 ((JDSRunnable)constructor.newInstance(null)).run( params );
+        	 }
+			
+		} catch (NoSuchMethodException e) {
+			e.printStackTrace();
+			
+		} catch (SecurityException e) {
+			e.printStackTrace();
+			
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			
+		} catch (InstantiationException e) {
+			e.printStackTrace();
+			
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+			
+		} catch (IllegalArgumentException e) {
+			e.printStackTrace();
+			
+		} catch (InvocationTargetException e) {
+			e.printStackTrace();
+		}
     }
     
 }
